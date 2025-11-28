b3959a1bf07f875ac23b0db0b662b9c336e6be9e
Asadseecsbese25 <masadrao426@gmail.com>
2025-11-28 14:56:22 +0500

    Added Guessing Game function by Asad

--- project.py
+++ project.py
@@ -21,7 +21,45 @@ def Saadain():
       if again != "yes":
         print("Good Bye !")
         break
-      
-Saadain()
+
+import random
+
+def Asad():
+    print("\t***** WELCOME to \"GUESS THE NUMBER!\" *****")
+    print("The number is between 1 and 50!\n")
+    x = random.randint(1, 50)
+    attempts = 1
+    while True:
+        try:
+            guess = int(input("Enter a number to start playing:\n"))
+        except ValueError:
+            print("Invalid input! Please enter a number.")
+            continue
+        if 1 <= guess <= 50:
+            if guess < x:
+                print("Too Low! Try Higher!\n")
+                attempts += 1
+            elif guess > x:
+                print("Too High! Try Lower!\n")
+                attempts += 1
+            else:
+                print(f"Congratulations! You guessed the number {x}!")
+                print(f"It took you {attempts} attempts.\n")
+                break
+        else:
+            print("Invalid Input. Enter a number between 1 and 50.\n")
+
+def main():
+    print("1. Smart Calculator")
+    print("2. Guessing Game")
+    choice = input("Choose option: ")
+    if choice == "1":
+        Saadain()
+    elif choice == "2":
+        Asad()
+    else:
+        print("Invalid choice")
+
+main()
 
 
